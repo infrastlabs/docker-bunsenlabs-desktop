965d6a89261d3bae5daa807ccbec39ce2015985a
sam#gemmi-win10 <sam@china.gz>
2025-06-01 06:38:43 +0800

    bs/img_build: img="docker-bunsenlabs-desktop:localbuild"

--- src/bunsenlabs-desktop/img_build.sh
+++ src/bunsenlabs-desktop/img_build.sh
@@ -9,7 +9,7 @@ repo=registry.cn-shenzhen.aliyuncs.com
 echo "${DOCKER_REGISTRY_PW_sdsir}" |docker login --username=${DOCKER_REGISTRY_USER_sdsir} --password-stdin $repo
 
 ns=infrastlabs
-img="docker-bunsen:latest"
+img="docker-bunsenlabs-desktop:localbuild"
 docker build --pull -t $repo/$ns/$img -f Dockerfile . 
 docker push $repo/$ns/$img
 
